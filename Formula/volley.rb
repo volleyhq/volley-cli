6da8521cb044a6aa51b6a20524778d05ce0169db
abhineet27 <abhineet.sitcsc@gmail.com>
2025-12-04 23:24:11 +0530

    fixed formula

--- Formula/volley.rb
+++ Formula/volley.rb
@@ -9,27 +9,39 @@ class Volley < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/volleyhq/volley-cli/releases/download/v0.1.0/volley-darwin-amd64.tar.gz"
-      sha256 ""
+      sha256 "83bd9d1b9f5d4ed9d6477159e1b2d671ffd6fe57f2415c58a0a19ffe968092d5"
     end
     if Hardware::CPU.arm?
       url "https://github.com/volleyhq/volley-cli/releases/download/v0.1.0/volley-darwin-arm64.tar.gz"
-      sha256 ""
+      sha256 "c490fa15f71cbd8f9ceff8d5ac9f7663253b66d18c2da29a9b908ba3b04bd817"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/volleyhq/volley-cli/releases/download/v0.1.0/volley-linux-amd64.tar.gz"
-      sha256 ""
+      sha256 "23eb48df61dea3f5ab981cc639104ab6f61f4389c809d352c52fd88993961519"
     end
     if Hardware::CPU.arm?
       url "https://github.com/volleyhq/volley-cli/releases/download/v0.1.0/volley-linux-arm64.tar.gz"
-      sha256 ""
+      sha256 "9c08959b167e9345c4806ed50bbd58f9588f231326703b740bdd1be2127230dc"
     end
   end
 
   def install
-    bin.install "volley"
+    if OS.mac?
+      if Hardware::CPU.intel?
+        bin.install "volley-darwin-amd64" => "volley"
+      else
+        bin.install "volley-darwin-arm64" => "volley"
+      end
+    else
+      if Hardware::CPU.intel?
+        bin.install "volley-linux-amd64" => "volley"
+      else
+        bin.install "volley-linux-arm64" => "volley"
+      end
+    end
   end
 
   test do
